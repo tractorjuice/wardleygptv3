a98ef7d8117a130582a408059b666ae1e99a8ac7
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-08-08 09:03:58 +0100

    Update app_faiss.py

--- app_faiss.py
+++ app_faiss.py
@@ -83,38 +83,6 @@ book_retriever = book_index.as_retriever(search_type="mmr", search_kwargs={"k": 
 # initialize the ensemble retriever
 ensemble_retriever = EnsembleRetriever(retrievers=[yt_retriever, book_retriever], weights=[0.5, 0.5])
 
-system_template="""
-    As a chatbot, analyze the provided videos on Wardley Mapping and offer insights and recommendations.
-    Suggestions:
-    Explain the analysis process for a Wardley Map
-    Discuss the key insights derived from the videos
-    Provide recommendations based on the analysis
-    Use the following pieces of context to answer the users question.
-    If you don't know the answer, just say that "I don't know", don't try to make up an answer.
-    ----------------
-    {summaries}"""
-prompt_messages = [
-    SystemMessagePromptTemplate.from_template(system_template),
-    HumanMessagePromptTemplate.from_template("{question}")
-    ]
-prompt = ChatPromptTemplate.from_messages(prompt_messages)
-
-chain_type_kwargs = {"prompt": prompt}
-llm = PromptLayerChatOpenAI(
-    model_name=MODEL,
-    temperature=0,
-    max_tokens=2000,
-    pl_tags=["wardleygptv2"],
-)  # Modify model_name if you have access to GPT-4
-
-chain = RetrievalQAWithSourcesChain.from_chain_type(
-    llm=llm,
-    chain_type="stuff",
-    retriever=vector_store.as_retriever(search_type="mmr", search_kwargs={"k": 3}), # Use MMR search and return 3 (max 20) video sources
-    return_source_documents=True,
-    chain_type_kwargs=chain_type_kwargs
-)
-
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
